d27bc128db582ca2250d3fe9a3d158526e5d8fbb
jcapellman <jcapellman@hotmail.com>
2015-04-22 22:59:24 -0400

    Started working on the User Agent filter logic

--- jcMWF.Test.MVC/Global.asax.cs
+++ jcMWF.Test.MVC/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.WebPages;
 
 namespace jcMWF.Test.MVC {
     public class MvcApplication : System.Web.HttpApplication {
@@ -13,6 +14,11 @@ namespace jcMWF.Test.MVC {
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var provider = new jcMWF.Lib.jcMWFProvider();
+
+            provider.Activate();
+
         }
     }
 }
