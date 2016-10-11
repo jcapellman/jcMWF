7863ea229e6331ce908ec97d7b22b9c14ffec12f
Jarred Capellman <jcapellman@hotmail.com>
2016-10-10 20:49:36 -0400

    Updated projects to 4.6.2

---
+++ jcMWF.Lib.Core/jcMWFProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Web.WebPages;
+
+namespace jcMWF.Lib.Core {
+    public class jcMWFProvider {
+        public void Activate(params object[] userAgents) {
+            DisplayModeProvider.Instance.Modes.Insert(0, new DefaultDisplayMode("classic") {
+                ContextCondition = (context => context.GetOverriddenUserAgent().IndexOf("netscape", StringComparison.OrdinalIgnoreCase) >= 0)
+            });
+        }
+    }
+}
